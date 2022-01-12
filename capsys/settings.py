013b5d0fdb95f435bf513e81b0ce62bf9aa77be8
johnmathewdino <55654524+johnmathewdino@users.noreply.github.com>
2022-01-13 00:34:31 +0800

    Update settings.py

--- capsys/settings.py
+++ capsys/settings.py
@@ -209,8 +209,8 @@ if DEBUG:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'huwanmatteo@gmail.com'
-    EMAIL_HOST_PASSWORD = 'ugll psjm onwt cxpp'
+    EMAIL_HOST_USER = 'INSERT EMAIL ADDRESS'
+    EMAIL_HOST_PASSWORD = 'INSERT PASSWORD'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
 
